3a1d08cb18d2ea931bc054fae8260f1d132b5415
Rahul Sharma <rahul__sharma@outlook.com>
2023-06-03 13:29:18 -0400

    feat: added socket connection

--- index.js
+++ index.js
@@ -14,7 +14,24 @@ app.use(cors())
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res)=> {
-    res.send('Server is running')
+    res.send('Server is runnings')
+})
+
+io.on('connection', (socket) => {   
+    socket.emit('me', socket.id)
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callended')
+    })
+
+    socket.on('calluser', ({userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit('calluser', {signal: signalData, from, name})
+    })
+
+    socket.on('answerCall', (data) => {
+        io.to(data.to).emit('callaccepted', data.signal)
+    })
+
 })
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
